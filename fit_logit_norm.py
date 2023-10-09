54452ef1a1c1a769d75955f751881ca6116fb577
cats256 <59489624+cats256@users.noreply.github.com>
2023-10-08 22:36:16 -0500

    Add logit-norm pdf plot

--- fit_logit_norm.py
+++ fit_logit_norm.py
@@ -5,12 +5,12 @@ from scipy.optimize import minimize
 import plotly.graph_objs as go
 
 
-def norm_loss(params, quantiles, scaled_observations):
+def norm_loss(params, quantiles, logit_observations):
     mean, std = params
-    expected_values = norm.ppf(quantiles, loc=mean, scale=std)
+    expected_values = norm.ppf(quantiles, mean, std)
 
-    squared_differences = (logit(scaled_observations) - expected_values) ** 2
-    # squared_differences = (scaled_observations - expit(expected_values)) ** 2
+    squared_differences = (logit_observations - expected_values) ** 2
+    # squared_differences = (expit(logit_observations) - expit(expected_values)) ** 2
 
     # not sure which loss function to use but both produces very similar results.
     # i personally prefer calculating the SSE by transforming the data back into the
@@ -33,19 +33,19 @@ upper_bound = 84
 scale = upper_bound - lower_bound
 
 quantiles = np.array([0.25, 0.50, 0.75])
-observed_values = np.array([56.87, 64.25, 70.59])
+observed_values = np.array([56.87, 64.25, 70.59]) - lower_bound
 
 logit_observations = logit(observed_values / scale)
-scaled_observations = observed_values / scale
 
 mean_estimate = np.mean(logit_observations)
 std_estimate = (logit_observations[-1] - logit_observations[0]) / (norm.ppf(quantiles[-1]) - norm.ppf(quantiles[0]))
 initial_parameters = np.array([mean_estimate, std_estimate])
 
+# L-BFGS-B method because loss function is convex
 optimization_result = minimize(
     norm_loss,
     initial_parameters,
-    args=(quantiles, scaled_observations),
+    args=(quantiles, logit_observations),
     method="L-BFGS-B",
     bounds=[(None, None), (0, None)],
 )
@@ -60,46 +60,76 @@ print("Expected Values:", expected_values)
 print("Sum Squared Error (Logit-Normal):", np.sum((observed_values - expected_values) ** 2))
 print("Sum Squared Error (Normal):", np.sum((logit_observations - expected_norm) ** 2))
 
-# mean_range = np.linspace(1.19354772770165 - 0.4, 1.19354772770165 + 0.4, 100)
-# std_range = np.linspace(0.01, 1.4, 100)
+# n_points = 200
+# # very restricted range but this shows how the function is convex more neatly
+# mean_range = np.linspace(mean - 0.4, mean + 0.4, n_points)
+# std_range = np.linspace(std - 0.6, std + 0.6, n_points)
 
-mean_range = np.linspace(mean - 0.4, mean + 0.4, 200)
-std_range = np.linspace(std - 0.6, std + 0.6, 200)
+# mean_grid, std_grid = np.meshgrid(mean_range, std_range)
 
-mean_grid, std_grid = np.meshgrid(mean_range, std_range)
+# params = np.stack((mean_grid, std_grid), axis=-1)
+# loss_values = np.apply_along_axis(lambda p: norm_loss(tuple(p), quantiles, logit_observations), -1, params)
 
-loss_values = np.zeros_like(mean_grid)
-for i in range(len(mean_range)):
-    for j in range(len(std_range)):
-        params = (mean_grid[i, j], std_grid[i, j])
-        loss_values[i, j] = norm_loss(params, quantiles, scaled_observations)
+# min_indices = np.unravel_index(loss_values.argmin(), loss_values.shape)
+# min_mean, min_std = mean_grid[min_indices], std_grid[min_indices]
 
-fig = go.Figure(data=[go.Surface(z=loss_values, x=mean_grid, y=std_grid)])
+# fig = go.Figure(data=[go.Surface(z=loss_values, x=mean_grid, y=std_grid, colorscale="Viridis")])
 
-min_indices = np.unravel_index(loss_values.argmin(), loss_values.shape)
+# fig.add_trace(
+#     go.Scatter3d(
+#         x=[min_mean],
+#         y=[min_std],
+#         z=[loss_values[min_indices]],
+#         mode="markers",
+#         marker=dict(size=5, color="red"),
+#         name="Minimum Point",
+#     )
+# )
 
-min_mean = mean_grid[min_indices]
-min_std = std_grid[min_indices]
+# fig.update_layout(
+#     scene=dict(
+#         xaxis_title="Mean",
+#         yaxis_title="Standard Deviation",
+#         zaxis_title="Loss",
+#         xaxis=dict(range=[mean - 0.4, mean + 0.4]),
+#         yaxis=dict(range=[std - 0.6, std + 0.6]),
+#     )
+# )
 
-fig.add_trace(
-    go.Scatter3d(
-        x=[min_mean],
-        y=[min_std],
-        z=[loss_values[min_indices]],
-        mode="markers",
-        marker=dict(size=5, color="red"),
-        name="Minimum Point",
-    )
-)
+# fig.write_html("logit_norm.html", auto_open=True)
 
-fig.update_layout(
-    scene=dict(
-        xaxis_title="Mean",
-        yaxis_title="Standard Deviation",
-        zaxis_title="Loss",
-        xaxis=dict(range=[mean - 0.4, mean + 0.4]),
-        yaxis=dict(range=[std - 0.6, std + 0.6]),
-    )
-)
+# contour = go.Contour(
+#     x=mean_range,
+#     y=std_range,
+#     z=loss_values,
+#     colorscale="Viridis",
+#     contours=dict(
+#         showlabels=True,
+#         labelfont=dict(size=12, color="white"),
+#     ),
+# )
 
-fig.write_html("logit_norm.html", auto_open=True)
+# min_point = go.Scatter(
+#     x=[min_mean],
+#     y=[min_std],
+#     mode="markers",
+#     marker=dict(size=10, color="red"),
+#     name="Minimum Point",
+# )
+
+# fig = go.Figure(data=[contour, min_point])
+
+# fig.update_layout(
+#     xaxis_title="Mean",
+#     yaxis_title="Standard Deviation",
+#     title="Loss Surface Contour Plot",
+# )
+
+# fig.write_html("contour_plot.html", auto_open=True)
+
+x_values = np.linspace(0, 1, 1000)
+y_values = norm.pdf(logit(x_values), mean, std)
+
+fig = go.Figure(data=go.Scatter(x=scale * x_values, y=y_values, mode="lines", name="Normal Distribution"))
+fig.update_layout(title="Estimated Grade Distribution (Scaled Logit Normal)", xaxis_title="X", yaxis_title="PDF")
+fig.write_html("logit_norm_pdf.html", auto_open=True)
