226d76e7b8f2eeecf974b5e60974bc6646c16299
cats256 <59489624+cats256@users.noreply.github.com>
2023-10-23 21:53:55 -0500

    Update ll function

--- assessment-statistics/flask-server/log_likelihood.py
+++ assessment-statistics/flask-server/log_likelihood.py
@@ -2,32 +2,20 @@ import numpy as np
 from scipy.optimize import differential_evolution
 from scipy.stats import expon, norm
 from scipy.optimize import dual_annealing
+from scipy.optimize import NonlinearConstraint
 
 
 def max_likelihood(params, observed_quantiles, quantiles):
-    try:
-        scale = params
-        log_likelihood = 0
+    scale = params
+    log_likelihood = 0
 
-        for i, quantile in enumerate(quantiles):
-            quantile_mean = expon.ppf(quantile, scale=scale)
-            quantile_std = np.sqrt(quantile * (1 - quantile) / (10000 * norm.pdf(norm.ppf(quantile)) ** 2))
+    for i, quantile in enumerate(quantiles):
+        quantile_mean = expon.ppf(quantile, scale=scale)
+        quantile_std = np.sqrt(quantile * (1 - quantile) / (10000 * norm.pdf(norm.ppf(quantile)) ** 2))
 
-            quantile_likelihood = norm.pdf(observed_quantiles[i], quantile_mean, quantile_std)
+        log_likelihood += norm.logpdf(observed_quantiles[i], quantile_mean, quantile_std)
 
-            if np.isnan(quantile_likelihood) or np.isinf(quantile_likelihood) or quantile_likelihood == 0:
-                return np.inf
-
-            log_q_prob = np.log(quantile_likelihood)
-
-            if np.isnan(log_q_prob) or np.isinf(log_q_prob):
-                return np.inf
-
-            log_likelihood += log_q_prob
-
-        return -log_likelihood
-    except:
-        return np.inf
+    return -log_likelihood
 
 
 def least_square_loss(params, observations, quantiles):
@@ -54,36 +42,51 @@ estimation_total_2 = np.zeros(1)
 estimation_loss_2 = np.zeros(1)
 
 rng = np.random.default_rng()
-true_params = np.array([1.98])
+true_params = np.array([1.38])
 size = 10000
-
 
 for i in range(num_loop):
     data = np.sort(rng.exponential(scale=1.38, size=size + 1))
     observed_quantiles = np.quantile(data, quantiles)
 
-    result = dual_annealing(
-        max_likelihood,
-        bounds=np.array([(1.0, 100.0)]),
-        args=(
-            observed_quantiles,
-            quantiles,
-        ),
-    )
-    estimation_total += result.x[:1]
-    estimation_loss += abs(true_params - result.x[:1]) ** 2
+    def custom_constraint(x):
+        try:
+            scale = x
+            log_likelihood = 0
 
-    result = dual_annealing(
-        least_square_loss,
-        bounds=np.array([(1.0, 100.0)]),
-        args=(
-            observed_quantiles,
-            quantiles,
-        ),
-    )
+            for i, quantile in enumerate(quantiles):
+                quantile_mean = expon.ppf(quantile, scale=scale)
+                quantile_std = np.sqrt(quantile * (1 - quantile) / (10000 * norm.pdf(norm.ppf(quantile)) ** 2))
+
+                quantile_likelihood = norm.pdf(observed_quantiles[i], quantile_mean, quantile_std)
+
+                if np.isnan(quantile_likelihood) or np.isinf(quantile_likelihood) or quantile_likelihood <= 1e-6:
+                    return -1
+
+                log_q_prob = np.log(quantile_likelihood)
+
+                if np.isnan(log_q_prob) or np.isinf(log_q_prob):
+                    return -1
+
+                log_likelihood += log_q_prob
+
+            return 5.0
+        except:
+            return -1
+
+    nonlin_constr = NonlinearConstraint(custom_constraint, lb=1.0, ub=100.0)
+
+    cons = {"type": "ineq", "fun": custom_constraint}
+
+    result = differential_evolution(max_likelihood, bounds=[(1.0, 100.0)], args=(observed_quantiles, quantiles))
+
+    estimation_total += result.x[:1]
+    estimation_loss += abs(true_params - result.x[:1]) ** 1
+
+    result = differential_evolution(least_square_loss, bounds=[(1.0, 100.0)], args=(observed_quantiles, quantiles))
 
     estimation_total_2 += result.x
-    estimation_loss_2 += abs(true_params - result.x) ** 2
+    estimation_loss_2 += abs(true_params - result.x) ** 1
 
     if i % 10 == 0 and i != 0:
         print(i)
